01ea33a8ebed59e11429706c48027ee85a649a91
Scott Harlan <seharlan@gmail.com>
2020-02-12 11:41:46 -0800

    second filter

--- photo-gallery/src/ImageList.js
+++ photo-gallery/src/ImageList.js
@@ -4,18 +4,28 @@ import ImageItem from './ImageItem.js';
 import ItemsArray from './data.js';
 
 export default class ImageList extends Component {
-    state = { selected: null };
+    state = { 
+        selected: null,
+        horns: null
+    };
     render() {
         const itemNodes = ItemsArray
             .filter(item => {
                 if (!this.state.selected) return true;
                 return item.keyword === this.state.selected;
             })
+            .filter(item => {
+                if (!this.state.horns) return true;
+                return item.horns === this.state.horns;
+            })
             .map(item => <ImageItem key={item.title} item={item} />);
 
         const handleChange = e => {
             this.setState({ selected: e.target.value });
         };
+        const handleHorns = e => {
+            this.setState({ horns: Number(e.target.value)});
+        }
         
         return <section>
             <select className="item-filter" onChange={handleChange}>
@@ -30,6 +40,14 @@ export default class ImageList extends Component {
                 <option value='addax'>Addax</option>
                 <option value='chameleon'>Chameleon</option>
                 <option value='dragon'>Dragon</option>
+            </select>
+
+            <select className='item-filter' onChange={handleHorns}>
+                <option value='' defaultValue>See All</option>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='100'>100</option>
             </select>
 
             <ul className='list'>
